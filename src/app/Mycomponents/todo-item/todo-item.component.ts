661181505025d1f1f9d922343215d332f11f1d5d
joychandragope <joychandragope@gmail.com>
2023-08-08 23:28:06 +0600

    todo list and delete button add

---
+++ src/app/Mycomponents/todo-item/todo-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Todo } from 'src/app/Todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css'],
+})
+export class TodoItemComponent {
+  @Input() todo: Todo;
+  onClick() {
+    console.log('OnClick Has Been Triggered');
+  }
+}
